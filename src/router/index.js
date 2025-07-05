e60e780c7f0da54b887cbd5f33d36aa4bb95399a
Lee Yi Terng <yiternglee02@gmail.com>
2025-07-05 13:55:43 +0900

    router and cleanup

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import PlaygroundView from '@/views/PlaygroundView.vue'
+import CocktailView from '../views/CocktailView.vue'
+import IngredientView from '../views/IngredientView.vue'
+import HmView from '../views/HmView.vue'
+import GlasswareView from '../views/GlasswareView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,12 +15,68 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/',
+      name: 'cocktail',
+      component: CocktailView,
+      children: [
+        {
+          path: 'create',
+        },
+        {
+          path: 'edit',
+        },
+        {
+          path: 'card',
+        },
+      ],
+    },
+    {
+      path: '/',
+      name: 'ingredient',
+      component: IngredientView,
+      children: [
+        {
+          path: 'create',
+        },
+        {
+          path: 'edit',
+        },
+        {
+          path: 'card',
+        },
+      ],
+    },
+    {
+      path: '/',
+      name: 'hm',
+      component: HmView,
+      children: [
+        {
+          path: 'create',
+        },
+        {
+          path: 'edit',
+        },
+        {
+          path: 'card',
+        },
+      ],
+    },
+    {
+      path: '/',
+      name: 'glassware',
+      component: GlasswareView,
+      children: [
+        {
+          path: 'create',
+        },
+        {
+          path: 'edit',
+        },
+        {
+          path: 'card',
+        },
+      ],
     },
         {
       path: '/playground',
