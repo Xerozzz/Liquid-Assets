c8d8979a5aa420cf0ff70c8353a355ad22237775
Lee Yi Terng <60227509+Xerozzz@users.noreply.github.com>
2025-07-20 14:36:28 +0900

    basic home style (#5)
    * basic home style
    
    * Update src/assets/main.css

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
@@ -7,7 +5,7 @@ import App from './App.vue'
 import router from './router'
 
 import './index.css'
-
+import './assets/main.css'
 const app = createApp(App)
 
 app.use(createPinia())
