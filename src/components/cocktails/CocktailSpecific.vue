133648eedf3580be4bc16c8858e92b9a3a913c29
Lee Yi Terng <yiternglee02@gmail.com>
2025-07-31 13:53:16 +0900

    Minor fixes

--- src/components/cocktails/CocktailSpecific.vue
+++ src/components/cocktails/CocktailSpecific.vue
@@ -47,49 +47,52 @@ export default {
 <template>
   <div v-if="loading">Loading...</div>
   <div v-else-if="error">{{ error.message }}</div>
-  <div v-else class="bodysection grid grid-cols-3 gap-11">
-    <div class="sectionbox">
-      <h2 class="title">{{ cocktail.recipe_name }}</h2>
-      <img
-        class="w-80 h-auto"
-        v-bind:src="cocktail.image"
-        alt="Cocktail Image"
-        v-if="cocktail.image"
-      />
-      <h3>Glass:</h3>
-      <p>{{ cocktail.glass_name }}</p>
-      <h3>Garnish:</h3>
-      <p>{{ cocktail.garnish }}</p>
-      <h3>Total Cost:</h3>
-      <p>${{ this.totalCost.toFixed(2) }}</p>
-      <h3>Notes:</h3>
-      <p>{{ cocktail.notes }}</p>
-    </div>
-    <div class="sectionbox">
-      <h3>Steps to make:</h3>
-      <p>{{ cocktail.step_to_make }}</p>
-    </div>
-    <div class="sectionbox">
-      <h3>Ingredients:</h3>
-      <p>Total Cost: ${{ this.totalCost.toFixed(2) }}</p>
-      <table class="text-center">
-        <thead>
-          <tr>
-            <th>Ingredient</th>
-            <th>Quantity</th>
-            <th>Cost</th>
-            <th>Stock Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="ingredient in ingredients" :key="ingredient.id">
-            <td>{{ ingredient.ingredient }}</td>
-            <td>{{ ingredient.quantity }}</td>
-            <td>${{ ingredient.cost.toFixed(2) }}</td>
-            <td>{{ ingredient.stock }}</td>
-          </tr>
-        </tbody>
-      </table>
+  <div v-else class="bodysection">
+    <button class="nav_button" @click="$router.push('/cocktail')">Back</button>
+    <div class="grid grid-cols-3 gap-11">
+      <div class="sectionbox">
+        <h2 class="title">{{ cocktail.recipe_name }}</h2>
+        <img
+          class="w-80 h-auto"
+          v-bind:src="cocktail.image"
+          alt="Cocktail Image"
+          v-if="cocktail.image"
+        />
+        <h3>Glass:</h3>
+        <p>{{ cocktail.glass_name }}</p>
+        <h3>Garnish:</h3>
+        <p>{{ cocktail.garnish }}</p>
+        <h3>Total Cost:</h3>
+        <p>${{ this.totalCost.toFixed(2) }}</p>
+        <h3>Notes:</h3>
+        <p>{{ cocktail.notes }}</p>
+      </div>
+      <div class="sectionbox">
+        <h3>Steps to make:</h3>
+        <p>{{ cocktail.step_to_make }}</p>
+      </div>
+      <div class="sectionbox">
+        <h3>Ingredients:</h3>
+        <p>Total Cost: ${{ this.totalCost.toFixed(2) }}</p>
+        <table class="text-center">
+          <thead>
+            <tr>
+              <th>Ingredient</th>
+              <th>Quantity</th>
+              <th>Cost</th>
+              <th>Stock Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="ingredient in ingredients" :key="ingredient.id">
+              <td>{{ ingredient.ingredient }}</td>
+              <td>{{ ingredient.quantity }}</td>
+              <td>${{ ingredient.cost.toFixed(2) }}</td>
+              <td>{{ ingredient.stock }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 </template>
