a95e0eec5212eab4e691b970893b93b7170d5287
caslawter <caslawter@gmail.com>
2025-08-02 21:40:01 +0800

    Fixed destructing issue with cocktails page not being found when imported in router/index.js

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,7 @@ import {
   CocktailEdit,
   CocktailSpecific,
   CocktailHome, 
-} from '@/components/cocktails'
+} from '@/components/cocktails/'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
