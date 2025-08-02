d9347c10fc71000142433ed18dd8f5bdfe140207
caslawter <caslawter@gmail.com>
2025-08-02 21:40:01 +0800

    Fixed destructing issue with cocktails page not being found when imported in router/index.js

---
+++ src/components/cocktails/index.js
@@ -0,0 +1,4 @@
+export { default as CocktailCreate } from './CocktailCreate.vue';
+export { default as CocktailEdit } from './CocktailEdit.vue';
+export { default as CocktailSpecific } from './CocktailSpecific.vue';
+export { default as CocktailHome } from './CocktailHome.vue';
