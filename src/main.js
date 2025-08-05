e15969724cec3cb78d500aa8063c28c3040c1728
caslawter <caslawter@gmail.com>
2025-08-05 16:35:30 +0800

    Added notification system

--- src/main.js
+++ src/main.js
@@ -3,6 +3,33 @@ import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config';
 import ToastService from 'primevue/toastservice';
 import Aura from '@primeuix/themes/aura';
+<<<<<<< HEAD
+=======
+
+import {
+    Accordion,
+    AccordionPanel,
+    AccordionHeader,
+    AccordionContent,
+    Button,
+    Card,
+    Column,
+    DataTable,
+    Tabs,
+    TabList,
+    Tab,
+    TabPanels,
+    TabPanel,
+    InputText,
+    IftaLabel,
+    Textarea,
+    Toast,
+    Divider,
+    FloatLabel,
+    Panel,
+    ProgressSpinner,
+} from 'primevue';
+>>>>>>> daea6d1 (Added notification system)
 
 
 import App from './App.vue'
@@ -20,6 +47,31 @@ app.use(PrimeVue, {
 app.use(createPinia())
 app.use(router)
 
+<<<<<<< HEAD
+=======
+app.component('InputText', InputText);
+app.component('Button', Button);
+app.component('IftaLabel', IftaLabel);
+app.component('InputText', InputText);
+app.component('Tabs', Tabs);
+app.component('Tab', Tab);
+app.component('TabList', TabList);
+app.component('TabPanels', TabPanels);
+app.component('TabPanel', TabPanel);
+app.component('Textarea', Textarea);
+app.component('Card', Card);
+app.component('Column', Column);
+app.component('DataTable', DataTable);
+app.component('AccordionContent', AccordionContent);
+app.component('Accordion', Accordion);
+app.component('AccordionPanel', AccordionPanel);
+app.component('AccordionHeader', AccordionHeader);
+app.component('Toast', Toast);
+app.component('Divider', Divider);
+app.component('FloatLabel', FloatLabel);
+app.component('Panel', Panel);
+app.component('ProgressSpinner', ProgressSpinner);
+>>>>>>> daea6d1 (Added notification system)
 app.use(ToastService);
 
 app.mount('#app')
