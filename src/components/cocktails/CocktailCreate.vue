daea6d150e01cd462fa07933037518fe7725fb0c
caslawter <caslawter@gmail.com>
2025-08-05 16:35:30 +0800

    Added notification system

--- src/components/cocktails/CocktailCreate.vue
+++ src/components/cocktails/CocktailCreate.vue
@@ -2,7 +2,16 @@
 import { getGlassware } from '@/api/glassware'
 import { getHmIngredient } from '@/api/hm_ingredients'
 import { getIngredients } from '@/api/ingredients'
+
+import { useSQLite } from '@/composables/useSQLite'
+import { useNotificationStore } from '@/stores/notification.store'
+
 export default {
+  setup() {
+    const notification = useNotificationStore()
+    const { destory } = useSQLite()
+    return { notification, destory }
+  },
   data() {
     return {
       ingredients: {},
@@ -18,13 +27,18 @@ export default {
   methods: {
     /** For some reason stacking the queries in 1 function call will cause sqllite to complain
      * and crash as it cannot handle multiple queries at once. Instead it needs to be seperated into
-     * its own functions so things may look a little ugly here. 
+     * its own functions so things may look a little ugly here.
      */
     async getIngredientData() {
       try {
         this.ingredients = await getIngredients()
       } catch (error) {
         console.log(error)
+        this.notification.notify({
+          message: `${error}`,
+          summary: 'Error',
+          severity: 'error',
+        })
       }
     },
     async getHmIngredientData() {
@@ -32,6 +46,11 @@ export default {
         this.hm_ingredients = await getHmIngredient()
       } catch (error) {
         console.log(error)
+        this.notification.notify({
+          message: `${error}`,
+          summary: 'Error',
+          severity: 'error',
+        })
       }
     },
     async getGlasswareData() {
@@ -39,6 +58,11 @@ export default {
         this.getGlassware = await getGlassware()
       } catch (error) {
         console.log(error)
+        this.notification.notify({
+          message: `${error}`,
+          summary: 'Error',
+          severity: 'error',
+        })
       }
     },
   },
@@ -46,8 +70,17 @@ export default {
     this.getIngredientData()
     this.getHmIngredientData()
     this.getGlasswareData()
+    this.notification.notify({
+      message: `test`,
+      summary: 'Test',
+    })
+  },
+  unmounted() {
+    this.destory()
   },
 }
 </script>
 
-<template></template>
+<template>
+  <h1>create</h1>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config';
+import ToastService from 'primevue/toastservice';
+import Aura from '@primeuix/themes/aura';
 
 import {
     Accordion,
@@ -18,7 +20,12 @@ import {
     TabPanel,
     InputText,
     IftaLabel,
-    Textarea
+    Textarea,
+    Toast,
+    Divider,
+    FloatLabel,
+    Panel,
+    ProgressSpinner,
 } from 'primevue';
 
 
@@ -29,12 +36,36 @@ import './index.css'
 import './assets/main.css'
 const app = createApp(App)
 app.use(PrimeVue, {
-    unstyled: true
+    theme: {
+        preset: Aura
+    }
 });
 
 app.use(createPinia())
 app.use(router)
 
 app.component('InputText', InputText);
+app.component('Button', Button);
+app.component('IftaLabel', IftaLabel);
+app.component('InputText', InputText);
+app.component('Tabs', Tabs);
+app.component('Tab', Tab);
+app.component('TabList', TabList);
+app.component('TabPanels', TabPanels);
+app.component('TabPanel', TabPanel);
+app.component('Textarea', Textarea);
+app.component('Card', Card);
+app.component('Column', Column);
+app.component('DataTable', DataTable);
+app.component('AccordionContent', AccordionContent);
+app.component('Accordion', Accordion);
+app.component('AccordionPanel', AccordionPanel);
+app.component('AccordionHeader', AccordionHeader);
+app.component('Toast', Toast);
+app.component('Divider', Divider);
+app.component('FloatLabel', FloatLabel);
+app.component('Panel', Panel);
+app.component('ProgressSpinner', ProgressSpinner);
+app.use(ToastService);
 
 app.mount('#app')
