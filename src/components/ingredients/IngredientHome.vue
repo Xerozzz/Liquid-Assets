17f91803c98256734fddef33229626ae41f38aac
Lee Yi Terng <60227509+Xerozzz@users.noreply.github.com>
2025-09-01 21:15:33 +0800

    baseplate

---
+++ src/components/ingredients/IngredientHome.vue
@@ -0,0 +1,66 @@
+<script>
+import { getIngredients } from '@/api/ingredients.js'
+import { useSQLite } from '@/composables/useSQLite'
+import { useNotificationStore } from '@/stores/notification.store'
+
+export default {
+  name: 'IngredientView',
+  setup() {
+    const notification = useNotificationStore()
+    const { destroy } = useSQLite()
+    return { notification, destroy }
+  },
+  data() {
+    return {
+      loading: true,
+      queryResult: [],
+      queryError: null,
+    }
+  },
+  methods: {
+    async retrieveIngredients() {
+      this.loading = true
+      this.queryResult = await getIngredients()
+      this.loading = false
+    },
+  },
+  mounted() {
+    this.retrieveIngredients()
+  },
+  unmounted() {
+    this.destroy()
+  },
+}
+</script>
+
+<template>
+  <div v-if="loading" class="text-center">
+    <p>Loading...</p>
+  </div>
+  <div class="bodysection" v-else>
+    <button class="nav_button" @click="$router.push('/')">Back</button>
+    <button class="nav_button" @click="$router.push('/ingredient/create')">
+      Create Ingredient
+    </button>
+    <h1 class="title">Ingredients</h1>
+    <div class="grid grid-cols-5 justify-center gap-8">
+      <div
+        class="border-1 rounded-md text-center pt-1"
+        v-for="ingredient in queryResult"
+        :key="ingredient.recipe_id"
+      >
+        <router-link :to="`/ingredients/view/${ingredient.recipe_id}`" class="font-bold text-md">{{
+          ingredient.name
+        }}</router-link>
+        <br />
+        <router-link :to="`/ingredients/view/${ingredient.recipe_id}`">
+          <img
+            v-bind:src="ingredient.image"
+            :alt="'Image of ' + ingredient.name"
+            v-if="ingredient.image"
+          />
+        </router-link>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/components/ingredients/index.js
@@ -0,0 +1,4 @@
+// export { default as IngredientCreate } from './IngredientCreate.vue';
+// export { default as IngredientEdit } from './IngredientEdit.vue';
+// export { default as IngredientSpecific } from './IngredientSpecific.vue';
+export { default as IngredientHome } from './IngredientHome.vue'
